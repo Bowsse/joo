4b1d01864dc06b733184c0b1178d9022f4b7fe83
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-18 02:18:37 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-adb start-server
-echo | android create avd --force -n android -t android-22 --abi armeabi-v7a
-screen -dm emulator -avd android -noaudio -no-window -gpu off -verbose -engine classic
+
 cd /home/work
 git clone https://github.com/IoTitude/Instapp.git  Instapp
 cd Instapp
@@ -11,10 +9,13 @@ latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latestTag
 npm install
 ionic resources android
-cordova platform add android
+ionic platform add android
 ionic state reset -- plugins
 ionic build android
 
+adb start-server
+screen -dm emulator -avd android -noaudio -no-window -gpu off -verbose -engine classic
+cd /home/work/appium
+screen -dm node .
 
-sleep 30
-adb devices
+
