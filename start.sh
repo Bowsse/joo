cab00c771f7b219024a947447dfa47cdbef9fef3
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-07-18 03:54:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,18 +4,32 @@
 cd /home/work
 git clone https://github.com/IoTitude/Instapp.git  Instapp
 cd Instapp
-git fetch --tags
-latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-git checkout $latestTag
+#git fetch --tags
+#latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+#git checkout $latestTag
 npm install
 ionic resources android
-ionic platform add android
+cordova platform add android
 ionic state reset -- plugins
 ionic build android
+
+echo 'Cloning test repo...'
+mkdir -p /home/test
+cd /home/test
+git init
+git remote add -f origin https://github.com/Bowsse/joo.git
+git config core.sparseCheckout true
+echo "robot.txt/" >> .git/info/sparse-checkout
+git pull origin master
 
 adb start-server
 screen -dm emulator -avd android -noaudio -no-window -gpu off -verbose -engine classic
 cd /home/work/appium
 screen -dm node .
+
+cd /home/test
+echo 'Starting robot tests...'
+robot robot.txt
+
 
 
